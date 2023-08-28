35d6fc25b81cf84f97d7adbe20b1295c268862a7
yuvraj-g <yuvrajwebdev@gmail.com>
2023-08-29 00:11:46 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default AuthLayout;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hi Discord Clone !</h1>
+      <Button>Discord</Button>
+    </main>
+  );
+}
