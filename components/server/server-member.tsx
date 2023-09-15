79e9a07161a2b35b6bdbad507a4194c4f5969acb
yubaraj-ulipsu <yubaraj@ulipsu.com>
2023-09-15 20:55:32 +0530

    created channel id pages and redirections. default to general redirection is implemented upon clicking on server.

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -25,9 +25,16 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 
   const icon = roleIconMap[member.role];
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+  }
+
+
+
   if (member.role !== "ADMIN")
     return (
       <button
+        onClick={onClick}
         className={cn(
           "group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
           params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -40,7 +47,7 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
         <p
           className={cn(
             "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-            params?.channelId === member.id &&
+            params?.memberId === member.id &&
               "text-primary dark:text-zinc-200 dark:group-hover:text-white"
           )}
         >
