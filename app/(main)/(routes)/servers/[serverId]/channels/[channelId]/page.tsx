2db9ea7eb5abf48376af4a689cae82d40c26e7c4
yubaraj-ulipsu <yubaraj@ulipsu.com>
2023-10-02 00:48:38 +0530

    added chat input and functionality. need to add api and rest..

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -35,18 +35,28 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   });
 
   if (!channel || !member) {
-    redirect("/")
+    redirect("/");
   }
 
-  return <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-    <ChatHeader
-      name={channel.name}
-      serverId={channel.serverId}
-      type="channel"
-    />
-    <div className="flex-1">Future Messages</div>
-    <ChatInput />
-  </div>;
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
